876590f85df7ec1d839e194511e1139a84da6fa3
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-04 06:24:41 +0000

    Use a CrudRepository instead of a PagingAndSortingRepository

--- json-invoice-processor/src/main/java/it/gov/aifa/invoice_processor/service/persistence/InvoiceVersionRepository.java
+++ json-invoice-processor/src/main/java/it/gov/aifa/invoice_processor/service/persistence/InvoiceVersionRepository.java
@@ -1,7 +1,7 @@
 package it.gov.aifa.invoice_processor.service.persistence;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import it.gov.aifa.invoice_processor.entity.invoice.InvoiceVersion;
 
-public interface InvoiceVersionRepository extends PagingAndSortingRepository<InvoiceVersion, String> {
+public interface InvoiceVersionRepository extends CrudRepository<InvoiceVersion, String> {
 }
