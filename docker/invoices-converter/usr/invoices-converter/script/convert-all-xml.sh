fb7c39d5d170f8f7ac677a0ad114e12af2af8576
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-05 13:09:37 +0200

    Post-process the purchase lines details. Close #9

--- docker/invoices-converter/usr/invoices-converter/script/convert-all-xml.sh
+++ docker/invoices-converter/usr/invoices-converter/script/convert-all-xml.sh
@@ -7,6 +7,7 @@ for X in $1/*; do
   output_file_name=$2/${output_file_name%$suffix}
   output_file_name_ext=${output_file_name}.json
   output_file_name_linked_invoice_data_ext=${output_file_name}_DatiFattureCollegate.json
+  output_file_name_purchase_lines_data_ext=${output_file_name}_DettaglioLinee.json
   output_file_name_post_processed_ext=${output_file_name}_post_processed.json
   echo "Processing: " $X
   xml2json -t xml2json --pretty -o $output_file_name_ext $X
@@ -16,6 +17,12 @@ for X in $1/*; do
     jq --slurpfile dati_fatture_collegate $output_file_name_linked_invoice_data_ext '."{http://www.fatturapa.gov.it/sdi/fatturapa/v1.1}FatturaElettronica".FatturaElettronicaBody.DatiGenerali.DatiFattureCollegate = $dati_fatture_collegate[0]' $output_file_name_ext >> $output_file_name_post_processed_ext
     rm $output_file_name_ext
     rm $output_file_name_linked_invoice_data_ext
+    mv $output_file_name_post_processed_ext $output_file_name_ext
+
+    jq '."{http://www.fatturapa.gov.it/sdi/fatturapa/v1.1}FatturaElettronica".FatturaElettronicaBody.DatiBeniServizi.DettaglioLinee' $output_file_name_ext | jq -s '.' > $output_file_name_purchase_lines_data_ext
+    jq --slurpfile dettaglio_linee $output_file_name_purchase_lines_data_ext '."{http://www.fatturapa.gov.it/sdi/fatturapa/v1.1}FatturaElettronica".FatturaElettronicaBody.DatiBeniServizi.DettaglioLinee = $dettaglio_linee[0]' $output_file_name_ext >> $output_file_name_post_processed_ext
+    rm $output_file_name_ext
+    rm $output_file_name_purchase_lines_data_ext
     mv $output_file_name_post_processed_ext $output_file_name_ext
   fi
 done
