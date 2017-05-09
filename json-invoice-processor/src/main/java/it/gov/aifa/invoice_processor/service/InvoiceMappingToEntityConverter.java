f11e5804cf6731b712b66c831d20ad03fdf3ebfd
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-09 10:28:45 +0000

    Typed the generic Invoice in InvoiceMappingToEntityConverter

--- json-invoice-processor/src/main/java/it/gov/aifa/invoice_processor/service/InvoiceMappingToEntityConverter.java
+++ json-invoice-processor/src/main/java/it/gov/aifa/invoice_processor/service/InvoiceMappingToEntityConverter.java
@@ -4,7 +4,7 @@ import org.springframework.core.convert.converter.Converter;
 
 public interface InvoiceMappingToEntityConverter
 <
-S extends it.gov.aifa.invoice_processor.mapping.Invoice
+S extends it.gov.aifa.invoice_processor.mapping.Invoice<String>
 ,T extends it.gov.aifa.invoice_processor.entity.invoice.Invoice
 > extends Converter<S,T>{
 
