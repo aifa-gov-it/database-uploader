95e5b154b4e6e72cf6f6f3ebe23fb02ffbacf967
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-11 06:44:55 +0000

    Removed unneeded InvoiceVersionRepository (and related test)

--- json-invoice-processor/src/main/java/it/gov/aifa/invoice_processor/service/persistence/InvoiceVersionRepository.java
+++
@@ -1,7 +0,0 @@
-package it.gov.aifa.invoice_processor.service.persistence;
-import org.springframework.data.repository.CrudRepository;
-
-import it.gov.aifa.invoice_processor.entity.invoice.InvoiceVersion;
-
-public interface InvoiceVersionRepository extends CrudRepository<InvoiceVersion, String> {
-}
