380c14341ddcc8f01e552db572f9d6b28c73862f
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-16 12:02:56 +0200

    Removed braces in the docker run command. For #14

--- script/build-database-uploader.sh
+++ script/build-database-uploader.sh
@@ -8,8 +8,8 @@ export DATABASE_UPLOADER_PROJECT_PATH=$TRAVIS_BUILD_DIR/database-uploader
 cd $DATABASE_UPLOADER_PROJECT_PATH
 echo "Building $DATABASE_UPLOADER_PROJECT_PATH"
 docker run --rm -it \
-  -v $(MAVEN_SETTINGS_SECURITY_PATH):/root/.m2/settings-security.xml \
-  -v $(DATABASE_UPLOADER_PROJECT_PATH):/usr/app \
+  -v $MAVEN_SETTINGS_SECURITY_PATH:/root/.m2/settings-security.xml \
+  -v $DATABASE_UPLOADER_PROJECT_PATH:/usr/app \
   -w /usr/app \
   -t maven:3.5.0-jdk-8-alpine \
   mvn clean install -s settings.xml
