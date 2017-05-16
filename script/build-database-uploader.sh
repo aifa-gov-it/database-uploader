d8e18215f97c1b4a809fec2b23968042d062c2a2
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-16 11:22:17 +0200

    Added echo statements to build script. For #13

--- script/build-database-uploader.sh
+++ script/build-database-uploader.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+
 export MAVEN_SETTINGS_SECURITY_PATH=$HOME/.m2/settings-security.xml
+echo "Setting up $MAVEN_SETTINGS_SECURITY_PATH"
 mkdir $HOME/.m2
 echo "$MAVEN_SETTINGS_SECURITY" > $MAVEN_SETTINGS_SECURITY_PATH
 export $DATABASE_UPLOADER_PROJECT_PATH=$TRAVIS_BUILD_DIR/database-uploader
 cd $DATABASE_UPLOADER_PROJECT_PATH
+echo "Building $DATABASE_UPLOADER_PROJECT_PATH"
 docker run --rm -it \
   -v $(MAVEN_SETTINGS_SECURITY_PATH):/root/.m2/settings-security.xml \
   -v $(DATABASE_UPLOADER_PROJECT_PATH):/usr/app \
