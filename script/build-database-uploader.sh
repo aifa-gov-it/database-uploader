142f08db1eb3e89f180407bf0ed316d97766f909
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-16 11:02:27 +0200

    Build database-uploader with a builder. For #13

---
+++ script/build-database-uploader.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export MAVEN_SETTINGS_SECURITY_PATH=$HOME/.m2/settings-security.xml
+mkdir $HOME/.m2
+echo "$MAVEN_SETTINGS_SECURITY" > $MAVEN_SETTINGS_SECURITY_PATH
+export $DATABASE_UPLOADER_PROJECT_PATH=$TRAVIS_BUILD_DIR/database-uploader
+cd $DATABASE_UPLOADER_PROJECT_PATH
+docker run --rm -it \
+  -v $(MAVEN_SETTINGS_SECURITY_PATH):/root/.m2/settings-security.xml \
+  -v $(DATABASE_UPLOADER_PROJECT_PATH):/usr/app \
+  -w /usr/app \
+  -t maven:3.5.0-jdk-8-alpine \
+  mvn clean install -s settings.xml
+docker build --rm -t aifagovit/database-uploader:latest .
