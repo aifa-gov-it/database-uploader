7cf97c47d3c0b9f9fe11d169e17ea219894f97f2
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-23 07:27:47 +0000

    Removed unused Test Constants. Close #7

--- database-uploader/src/test/java/it/gov/aifa/invoice_processor/constant/TestConstant.java
+++ database-uploader/src/test/java/it/gov/aifa/invoice_processor/constant/TestConstant.java
@@ -1,26 +1,8 @@
 package it.gov.aifa.invoice_processor.constant;
 
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public final class TestConstant {
-	private static final Logger log = LoggerFactory.getLogger(TestConstant.class);
-	private TestConstant() { }
-	
-	public static final String TEST_DIRECTORY_NAME = "input";
-	public static final Path TEST_DIRECTORY_PATH = getPath(TEST_DIRECTORY_NAME);
+	public static final String TEST_DIRECTORY_PATH_INVOICE_XML = "input/invoice-xml/IT*.xml";
 	public static final String TEST_FILE_PATH_MOVEMENT_DSV = "src/test/resources/input/movement-dsv/movements.dsv";
 	
-	public static Path getPath(String path){
-		try {
-			return Paths.get(TestConstant.class.getClassLoader().getResource(path).toURI());
-		} catch (URISyntaxException e) {
-			log.error("Error while getting {} path", path, e);
-			throw new RuntimeException("Cannot get path " + path);
-		}
-	}
+	private TestConstant() { }
 }
