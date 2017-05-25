7731a592bcd6f71b0fb743a9f6aa5fe9b2b063cc
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-25 08:28:43 +0000

    Added echoes to build-database-uploader.sh

--- script/build-database-uploader.sh
+++ script/build-database-uploader.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+# See https://github.com/travis-ci/travis-ci/issues/1066
+# Fail if one of the commands of this script fails
+set -e
+
+echo "Setting up Maven settings-security.xml"
 export MAVEN_SETTINGS_SECURITY_PATH=$HOME/.m2/settings-security.xml
-echo "Setting up $MAVEN_SETTINGS_SECURITY_PATH"
+echo "Set MAVEN_SETTINGS_SECURITY_PATH to $MAVEN_SETTINGS_SECURITY_PATH"
 mkdir -p $HOME/.m2
 echo "$MAVEN_SETTINGS_SECURITY" > $MAVEN_SETTINGS_SECURITY_PATH
+
+echo "Setting up DATABASE_UPLOADER_PROJECT_PATH"
 export DATABASE_UPLOADER_PROJECT_PATH=$TRAVIS_BUILD_DIR/database-uploader
+echo "Set DATABASE_UPLOADER_PROJECT_PATH to $DATABASE_UPLOADER_PROJECT_PATH"
 cd $DATABASE_UPLOADER_PROJECT_PATH
 echo "Building $DATABASE_UPLOADER_PROJECT_PATH"
 docker run --rm -it \
@@ -13,4 +21,9 @@ docker run --rm -it \
   -w /usr/app \
   -t maven:3.5.0-jdk-8-alpine \
   mvn clean install -s settings.xml
-docker build --rm -t aifagovit/database-uploader:latest .
+
+export RUNTIME_CONTAINER_ID="aifagovit/database-uploader"
+echo "Building $RUNTIME_CONTAINER_ID"
+docker build --rm -t $RUNTIME_CONTAINER_ID:latest .
+
+set +e
