e5692e78eb531fb5651b697c3d61ad842d7c599f
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-25 16:25:50 +0000

    Removed unnecessary Spring Boot profile activation during IT

--- database-uploader/src/test/java/it/gov/aifa/invoice_processor/AbstractIT.java
+++ database-uploader/src/test/java/it/gov/aifa/invoice_processor/AbstractIT.java
@@ -2,10 +2,8 @@ package it.gov.aifa.invoice_processor;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
-@ActiveProfiles(profiles = { "embeddedDatabase"})
 @SpringBootTest(
         classes = {ContextConfig.class}
         ,webEnvironment = WebEnvironment.NONE
