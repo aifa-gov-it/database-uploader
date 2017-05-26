9d27ef644a4c07b7de8bfff5af1fbe2b481006d3
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-26 12:44:31 +0000

    cd to database-uploader before building database-uploader

--- script/build-database-uploader.sh
+++ script/build-database-uploader.sh
@@ -4,22 +4,22 @@
 # Fail if one of the commands of this script fails
 set -e
 
+cd $TRAVIS_BUILD_DIR/database-uploader
+
 echo "Building aifa-gov-it/database-uploader"
-docker build --rm --file=$TRAVIS_BUILD_DIR/database-uploader/Dockerfile-build -t aifa-gov-it/database-uploader-build:latest .
+docker build --rm --file=Dockerfile-build -t aifa-gov-it/database-uploader-build:latest .
 
 echo "Building database-uploader"
 docker run --rm -it \
   -v $TRAVIS_BUILD_DIR:/usr/app \
   -w /usr/app/database-uploader \
-  -t aifa-gov-it/database-uploader-build:lates \
+  -t aifa-gov-it/database-uploader-build:latest \
   mvn clean install \
     jacoco:report jacoco:report-integration \
     coveralls:report \
     sonar:sonar \
     -Dbranch=$TRAVIS_BRANCH -DpullRequest=$TRAVIS_PULL_REQUEST -DserviceJobId=$TRAVIS_JOB_ID -DserviceName="travis-ci" \
     -Dsonar.host.url=https://sonarqube.com -Dsonar.organization=aifa-gov-it -Dsonar.login=$SONARQUBE_LOGIN_TOKEN -Dsonar.branch=$TRAVIS_BRANCH
-
-cd $TRAVIS_BUILD_DIR/database-uploader
 
 echo "Try to run aifagovit/database-uploader"
 docker build --rm -t aifa-gov-it/database-uploader:latest .
