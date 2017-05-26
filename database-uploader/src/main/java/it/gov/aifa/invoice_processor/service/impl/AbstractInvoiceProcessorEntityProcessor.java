760b2ba7f84a6294958556c3b628c5304c5418a0
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-26 14:46:46 +0000

    Log entity id after updating it

--- database-uploader/src/main/java/it/gov/aifa/invoice_processor/service/impl/AbstractInvoiceProcessorEntityProcessor.java
+++ database-uploader/src/main/java/it/gov/aifa/invoice_processor/service/impl/AbstractInvoiceProcessorEntityProcessor.java
@@ -1,5 +1,7 @@
 package it.gov.aifa.invoice_processor.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 
 import it.gov.aifa.invoice_processor.entity.InvoiceProcessorEntity;
@@ -8,11 +10,13 @@ public abstract class AbstractInvoiceProcessorEntityProcessor<
 I
 , O extends InvoiceProcessorEntity<String>
 > implements ItemProcessor<I, O> {
+	private static final Logger log = LoggerFactory.getLogger(AbstractInvoiceProcessorEntityProcessor.class);
 
 	@Override
 	public final O process(final I item) throws Exception {
 		O output = processInternal(item);
 		output.updateId();
+		log.info("Processed entity {}: {}", output.getClass().toGenericString(), output.getId());
 		return output;
 	}
 	
--- database-uploader/src/main/java/it/gov/aifa/invoice_processor/service/impl/MovementProcessor.java
+++ database-uploader/src/main/java/it/gov/aifa/invoice_processor/service/impl/MovementProcessor.java
@@ -1,7 +1,5 @@
 package it.gov.aifa.invoice_processor.service.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import it.gov.aifa.invoice_processor.entity.movement.Movement;
@@ -9,11 +7,8 @@ import it.gov.aifa.invoice_processor.entity.movement.Movement;
 @Service
 public class MovementProcessor
 extends AbstractInvoiceProcessorEntityProcessor<Movement, Movement>{
-	private static final Logger log = LoggerFactory.getLogger(MovementProcessor.class);
-	
 	@Override
 	protected Movement processInternal(final Movement movement) {
-		log.info("Processed movement: {}", movement.getId());
 		return movement;
 	}
 }
