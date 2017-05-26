40611744951f6477acbc1eba2c4be3d361f515e0
Marco Ferrari <ferrari.marco@gmail.com>
2017-05-26 12:39:55 +0000

    Hopefully fixed build scripts

--- script/build-database-uploader.sh
+++ script/build-database-uploader.sh
@@ -5,7 +5,7 @@
 set -e
 
 echo "Building aifa-gov-it/database-uploader"
-docker build --rm --file=Dockerfile-build -t aifa-gov-it/database-uploader-build:latest .
+docker build --rm --file=$TRAVIS_BUILD_DIR/database-uploader/Dockerfile-build -t aifa-gov-it/database-uploader-build:latest .
 
 echo "Building database-uploader"
 docker run --rm -it \
@@ -18,6 +18,8 @@ docker run --rm -it \
     sonar:sonar \
     -Dbranch=$TRAVIS_BRANCH -DpullRequest=$TRAVIS_PULL_REQUEST -DserviceJobId=$TRAVIS_JOB_ID -DserviceName="travis-ci" \
     -Dsonar.host.url=https://sonarqube.com -Dsonar.organization=aifa-gov-it -Dsonar.login=$SONARQUBE_LOGIN_TOKEN -Dsonar.branch=$TRAVIS_BRANCH
+
+cd $TRAVIS_BUILD_DIR/database-uploader
 
 echo "Try to run aifagovit/database-uploader"
 docker build --rm -t aifa-gov-it/database-uploader:latest .
