4cd5bcaae03676b8e3ee1dc6dbd03d0b06dc6955
Marco Ferrari <ferrari.marco@gmail.com>
2017-06-22 09:55:07 +0000

    Annotated movementUniqueConstraintViolationTest with @DirtiesContext

--- src/test/java/it/gov/aifa/invoice_processor/entity/movement/MovementIT.java
+++ src/test/java/it/gov/aifa/invoice_processor/entity/movement/MovementIT.java
@@ -2,6 +2,7 @@ package it.gov.aifa.invoice_processor.entity.movement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.annotation.DirtiesContext;
 import org.testng.annotations.Test;
 
 import it.gov.aifa.invoice_processor.AbstractIT;
@@ -12,6 +13,7 @@ public class MovementIT extends AbstractIT{
 	@Autowired
 	private MovementRepository movementRepository;
 	
+	@DirtiesContext
 	@Test(expectedExceptions = { DataIntegrityViolationException.class })
 	public void movementUniqueConstraintViolationTest() {
 		Movement m = new Movement("aic", "documentNumber", "documentTypeCode", "lot", "2017-06-22", "11:33:34");
