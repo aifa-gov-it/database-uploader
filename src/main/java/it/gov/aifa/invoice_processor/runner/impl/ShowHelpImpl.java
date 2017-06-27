3c8640e83e50703b3f5f837052eb85d4e733e626
Marco Ferrari <ferrari.marco@gmail.com>
2017-06-27 08:00:29 +0000

    Check if there are only unrecognized arguments. Close #24

--- src/main/java/it/gov/aifa/invoice_processor/runner/impl/ShowHelpImpl.java
+++ src/main/java/it/gov/aifa/invoice_processor/runner/impl/ShowHelpImpl.java
@@ -1,6 +1,7 @@
 package it.gov.aifa.invoice_processor.runner.impl;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +12,18 @@ public class ShowHelpImpl implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		if(args == null || ArrayUtils.isEmpty(args)) {
+		// Check if args contains only unrecognized options
+		boolean onlyUnrecognized = true;
+		for(String arg : args)
+			for(CommandLineTask commandLineTask : CommandLineTask.values())
+				if(onlyUnrecognized && StringUtils.isNotBlank(arg) && commandLineTask.getArgumentKey().equals(arg))
+					onlyUnrecognized = false;
+		
+		if(args == null || ArrayUtils.isEmpty(args) || onlyUnrecognized) {
 			StringBuilder message = new StringBuilder();
 			message.append("Database Uploader: upload data from source data files to the configured database.");
+			message.append(System.lineSeparator());
+			message.append("Unrecognized options will be ignored.");
 			message.append(System.lineSeparator());
 			message.append("OPTIONS");
 			message.append(System.lineSeparator());
--- src/test/java/it/gov/aifa/invoice_processor/runner/impl/ShowHelpImplTest.java
+++ src/test/java/it/gov/aifa/invoice_processor/runner/impl/ShowHelpImplTest.java
@@ -14,12 +14,42 @@ import it.gov.aifa.invoice_processor.runner.CommandLineTask;
 public class ShowHelpImplTest {
 	
 	@Test
+	public void runRecognizedArgumentsTest() throws Exception {
+		OutputStream outContent = new ByteArrayOutputStream();
+		PrintStream originalStream = System.out;
+		System.setOut(new PrintStream(outContent));
+		CommandLineRunner showHelp = new ShowHelpImpl();
+		String[] arguments = new String[1];
+		arguments[0] = CommandLineTask.HELP.getArgumentKey();
+		showHelp.run(arguments);
+		String sysoutContent = outContent.toString();
+	    assertThat(sysoutContent).isEmpty();
+	    System.setOut(originalStream);
+	}
+	
+	@Test
 	public void runNullArgumentsTest() throws Exception {
 		OutputStream outContent = new ByteArrayOutputStream();
 		PrintStream originalStream = System.out;
 		System.setOut(new PrintStream(outContent));
 		CommandLineRunner showHelp = new ShowHelpImpl();
 		showHelp.run(new String[0]);
+		String sysoutContent = outContent.toString();
+	    assertThat(sysoutContent).isNotEmpty();
+	    for(CommandLineTask commandLineTask : CommandLineTask.values())
+	    	assertThat(sysoutContent).contains(commandLineTask.getArgumentKey());
+	    System.setOut(originalStream);
+	}
+	
+	@Test
+	public void runUnrecognizedArgumentsOnlyTest() throws Exception {
+		OutputStream outContent = new ByteArrayOutputStream();
+		PrintStream originalStream = System.out;
+		System.setOut(new PrintStream(outContent));
+		CommandLineRunner showHelp = new ShowHelpImpl();
+		String[] arguments = new String[1];
+		arguments[0] = "--dummy-arg";
+		showHelp.run(arguments);
 		String sysoutContent = outContent.toString();
 	    assertThat(sysoutContent).isNotEmpty();
 	    for(CommandLineTask commandLineTask : CommandLineTask.values())
