2271cd7148451c08cc49be561e5a5ca71b714a36
Marco Ferrari <ferrari.marco@gmail.com>
2017-12-20 11:35:11 +0000

    Fix arguments setup for the startup script

--- start-database-uploader.sh
+++ start-database-uploader.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -e
 
+ARGS="$@"
+echo "Database uploader started with arguments: $ARGS"
+
 # some IBAN codes are not valid according to the XSD, let's fix this
 find "$WORKDIR" -type f -print0 -iname "*.xml" | xargs -0 sed -i 's/XXXXXXXXXXXXXXX/XX00XXXXXXXXXXX/g'
 
-java -jar database-uploader-exec.jar "$?"
+java -jar database-uploader-exec.jar "$ARGS"
