00a9b4e4259d1507dbb25404302259eb7513a938
Marco Ferrari <ferrari.marco@gmail.com>
2017-12-20 10:56:54 +0000

    Add arguments to startup script

--- start-database-uploader.sh
+++ start-database-uploader.sh
@@ -3,4 +3,4 @@
 # some IBAN codes are not valid according to the XSD, let's fix this
 find "$WORKDIR" -type f -print0 -iname "*.xml" | xargs -0 sed -i 's/XXXXXXXXXXXXXXX/XX00XXXXXXXXXXX/g'
 
-java -jar database-uploader-exec.jar
+java -jar database-uploader-exec.jar "$?"
