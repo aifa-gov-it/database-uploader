4f9929de2e44c5d5284814b1de278a33a1f6ce1b
Marco Ferrari <ferrari.marco@gmail.com>
2017-12-20 13:02:35 +0000

    Fix SC2086 SC2140

--- script/build-database-uploader.sh
+++ script/build-database-uploader.sh
@@ -6,7 +6,7 @@ set -e
 
 echo "Building database-uploader"
 docker run --rm -it \
-  -v $TRAVIS_BUILD_DIR:/usr/app \
+  -v "$TRAVIS_BUILD_DIR":/usr/app \
   -w /usr/app \
   -t maven:3.5.0-jdk-8-alpine \
   /bin/ash -c "
@@ -15,7 +15,7 @@ docker run --rm -it \
   && mvn clean install \
     jacoco:report jacoco:report-integration \
     sonar:sonar \
-    -Dbranch=$TRAVIS_BRANCH -DpullRequest=$TRAVIS_PULL_REQUEST -DserviceJobId=$TRAVIS_JOB_ID -DserviceName="travis-ci" \
+    -Dbranch=$TRAVIS_BRANCH -DpullRequest=$TRAVIS_PULL_REQUEST -DserviceJobId=$TRAVIS_JOB_ID -DserviceName='travis-ci' \
     -Dsonar.host.url=https://sonarqube.com -Dsonar.organization=aifa-gov-it -Dsonar.login=$SONARQUBE_LOGIN_TOKEN -Dsonar.branch=$TRAVIS_BRANCH
   "
 
